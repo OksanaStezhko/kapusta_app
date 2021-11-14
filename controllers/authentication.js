49ba9e2af38c26e497b476d27dea8449c51349d9
OksanaStezhko <oksana_stegko@ukr.net>
2021-11-14 21:52:46 +0200

    исправила отбор по пользовтелю для роута reports

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -59,6 +59,7 @@ const googleRedirect = async (req, res) => {
 
   const token = currentUser.createToken()
   await User.findByIdAndUpdate(currentUser._id, { token })
+
   return res.redirect(
     `${FRONTEND_URL}/authorized?token = ${token}&email=${email}&name=${name}`
   )
